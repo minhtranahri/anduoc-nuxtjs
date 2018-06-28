4c486360824dc2aa090ba551d4d4f79a6c234e10
minhtranahri <creepoftheyear@gmail.com>
2018-06-28 21:54:46 +0700

    ssr

--- pages/News.vue
+++ pages/News.vue
@@ -4,13 +4,13 @@
       <div class="wrap-content w100 fl">
         <div class="clear-fix"></div>
         <div class="new_content">
-          <breadcrumb v-if="this.$store.state.loaded" :breadcrumb="breadcrumb"/>
+          <breadcrumb :breadcrumb="breadcrumb"/>
           <div class="clear-fix">
           </div>
-          <top-news v-if="this.$store.state.loaded" :topnews="topnews"/>
-          <bottom-child v-if="this.$store.state.loaded" :bottomnews="bottomnews"/>
+          <top-news :topnews="topnews"/>
+          <bottom-child :bottomnews="bottomnews"/>
         </div>
-        <side-bar v-if="this.$store.state.loaded" :sidebar="sidebar"/>
+        <side-bar :sidebar="sidebar"/>
       </div>
     </div>
   </section>
@@ -34,20 +34,32 @@
           sidebar: null,
         }
       },
-      
-      mounted() {
-        this.$axios
-          .get('http://dev.anduoc.vn/api/news')
-          .then(response => {
-            this.breadcrumb = response.data.data.breadcrumb;
-            this.topnews = response.data.data;
-            this.bottomnews = response.data.data.cate_news;
-            this.sidebar = response.data.data.product;
-            if(response){
-              this.$store.commit('toggleLoadingStatus');
-            }
-          })
-      },
+
+      async asyncData({app}){
+        let [news] = await Promise.all([
+          app.$axios.get('http://dev.anduoc.vn/api/news')
+        ])
+        return {
+          breadcrumb: news.data.data.breadcrumb,
+          topnews: news.data.data,
+          bottomnews: news.data.data.cate_news,
+          sidebar: news.data.data.product,
+        }
+      }
+
+      // mounted() {
+      //   this.$axios
+      //     .get('http://dev.anduoc.vn/api/news')
+      //     .then(response => {
+      //       this.breadcrumb = response.data.data.breadcrumb;
+      //       this.topnews = response.data.data;
+      //       this.bottomnews = response.data.data.cate_news;
+      //       this.sidebar = response.data.data.product;
+      //       if(response){
+      //         this.$store.commit('toggleLoadingStatus');
+      //       }
+      //     })
+      // },
 
       // beforeMount() {
       //     if(this.$store.state.loaded == true){
