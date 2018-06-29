c02d11918221beccfd1e091b70b59480149a96df
minhtranahri <creepoftheyear@gmail.com>
2018-06-29 08:54:03 +0700

    ssr

---
+++ components/loading.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="loadingAnimation" v-if="loading">
+    <img style="width: 100px" src="/images/giphy.gif">
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "loading",
+      data: () => ({
+        loading: false
+      }),
+      methods: {
+        start () {
+          this.loading = true
+        },
+        finish () {
+          this.loading = false
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .loadingAnimation{
+    text-align: center;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center; position: fixed;z-index: 1000; top: 0; flex-direction: column;
+  }
+</style>
--- layouts/Header.vue
+++ layouts/Header.vue
@@ -1,8 +1,5 @@
 <template>
   <div>
-    <!--<div v-if="this.$store.state.loaded != true" class="loadingAnimation">-->
-      <!--<img style="width: 100px" src="/images/giphy.gif">-->
-    <!--</div>-->
       <app-header :class="this.$route.path == '/'?'home-anduoc':''" v-if="keyobj" :key_obj="keyobj"/>
       <nuxt/>
       <app-footer/>
@@ -24,14 +21,14 @@
         }
       },
       methods: {
-          showProgress: function () {
-            if(!this.$store.state.loaded){
-              this.$nuxt.$loading.start();
-            }
-            else{
-              this.$nuxt.$loading.finish();
-            }
-          }
+          // showProgress: function () {
+          //   if(!this.$store.state.loaded){
+          //     this.$nuxt.$loading.start();
+          //   }
+          //   else{
+          //     this.$nuxt.$loading.finish();
+          //   }
+          // }
       },
       mounted() {
         // this.$nextTick(() => {
@@ -48,12 +45,5 @@
 </script>
 
 <style scoped>
-  .loadingAnimation{
-    text-align: center;
-    width: 100vw;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center; position: fixed;z-index: 1000; top: 0; flex-direction: column;
-  }
+
 </style>
